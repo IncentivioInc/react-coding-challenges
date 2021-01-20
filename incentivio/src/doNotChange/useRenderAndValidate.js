f9fd2fefd00281b96defae37618c15b9ad9b37ce
Robert McCracken <mccracken.bob@gmail.com>
2021-01-20 15:55:28 -0500

    Update useRenderAndValidate.js

--- incentivio/src/doNotChange/useRenderAndValidate.js
+++ incentivio/src/doNotChange/useRenderAndValidate.js
@@ -1,7 +1,9 @@
+import { useRef } from "react";
 import { useDispatch, useSelector } from "react-redux";
 import { setLoadingError, setMatchError } from "./results.slice";
 
 export const useRenderAndValidate = (char, stateChar) => {
+  const renderCount = useRef(0);
   const dispatch = useDispatch();
   const allCharactersLoaded = useSelector((state) => {
     const characters = ["i", "n", "c", "e", "t", "v", "o"];
@@ -9,12 +11,14 @@ export const useRenderAndValidate = (char, stateChar) => {
   });
 
   return function renderAndValidate() {
-    if (!allCharactersLoaded) {
-      dispatch(setLoadingError());
-    }
+    if (renderCount.current > 0) {
+      if (!allCharactersLoaded) {
+        dispatch(setLoadingError());
+      }
 
-    if (char !== stateChar) {
-      dispatch(setMatchError({ expected: char, actual: stateChar }));
+      if (char !== stateChar) {
+        dispatch(setMatchError({ expected: char, actual: stateChar }));
+      }
     }
 
     return stateChar;
