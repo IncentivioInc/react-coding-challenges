73dea86051109280ca64a6547982d879e74350ef
Robert McCracken <mccracken.bob@gmail.com>
2021-01-20 15:56:31 -0500

    Update App.hooks.js

--- incentivio/src/App.hooks.js
+++ incentivio/src/App.hooks.js
@@ -3,7 +3,7 @@ import { useSelector } from "react-redux";
 export const useCharacters = () => {
   // use your thunks here
 
-  // This should be true when all characters have been loaded
+  // This should be false when all characters have been loaded
   const loading = useSelector((state) => false);
 
   return {
